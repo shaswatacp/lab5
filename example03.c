2e8d62f5281e19e31c06ee0b92c3c6af247e0a1a
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-24 20:22:55 +0600

    Add files via upload

---
+++ example03.c
@@ -0,0 +1,16 @@
+//A c program to demonostrate the use of continue statement.
+#include<stdio.h>
+int main()
+{
+    int i,n=20;
+    for(i=1;i<=n;i++)
+    {
+        if(i%5==0)
+        {
+            printf("pass\n");
+            continue;
+        }
+        printf("%d",i);
+    }
+
+}
